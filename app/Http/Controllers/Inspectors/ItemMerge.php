b3ea346ffe304ca1dfd3de20fae8e73396307729
jkga <johnkennethgibasabella@gmail.com>
2019-10-09 15:12:25 +0800

    Added AST Inspector for line item per cost center

--- app/Http/Controllers/Inspectors/ItemMerge.php
+++ app/Http/Controllers/Inspectors/ItemMerge.php
@@ -31,6 +31,59 @@ class ItemMerge extends Controller
             echo 'Invalid Account';
             exit;
         }
+
+        # build HTML files
+        $__spacer = '&emsp;&emsp;';
+        $__breaker = '<br/>';
+        $__HTML = '<style>body { padding: 50px; font-size:12.5px; }</style>';
+        $__HTML.= '<h3>AST Inspector</h3><p>Tree View for Line Item per Cost Center</p><hr/><br/><br/>';
+
+        # query HELL
+        foreach($this->ast as $key => $val) {  echo '<br/><br/>';
+            $__HTML.="<details open><summary>Cost Center{$__spacer}{$key}</summary>{$__breaker}";
+
+            foreach($val as $line_key => $line_val) {
+                $__HTML.="{$__breaker}<details open><summary>{$__spacer}{$__spacer} LINE ITEM: <b>{$line_key}</b></summary>{$__breaker}";
+                foreach($line_val as $fy_key => $fy_val) {
+                    $__HTML.="{$__breaker }{$__spacer}{$__spacer} 
+                                {$__spacer}{$__spacer}<details open><summary>{$__spacer}{$__spacer}{$__spacer}{$__spacer}
+                                {$__spacer}<b> </b> <b>{$fy_key}</b></summary>{$__breaker}";
+                    # activities
+                    foreach($fy_val->activities as $key_act => $val_act) {  
+                        $__HTML.="{$__breaker }{$__spacer}{$__spacer}{$__spacer}{$__spacer}{$__spacer}{$__spacer} 
+                            {$__spacer}{$__spacer}<details open><summary>{$__spacer}{$__spacer}{$__spacer}{$__spacer}{$__spacer}{$__spacer}{$__spacer}{$__spacer}
+                            Activity: {$__spacer}<b> </b> <b>{$key_act}</b></summary>{$__breaker}";
+
+                            foreach($val_act as $key_budg => $val_budg) {
+                                $__HTML.="{$__breaker}{$__spacer}{$__spacer} 
+                                {$__spacer}{$__spacer}<details open>
+                                <summary>
+                                    {$__spacer}{$__spacer}{$__spacer}{$__spacer}{$__spacer}{$__spacer}{$__spacer}{$__spacer}{$__spacer}{$__spacer}{$__spacer}
+                                    <u>Requirement #{$val_budg->lineid}</u>{$__spacer} <b>{$val_budg->lineitem}</b></summary>{$__breaker}
+                                    {$__spacer}{$__spacer}{$__spacer}{$__spacer}{$__spacer}{$__spacer}{$__spacer}{$__spacer}{$__spacer}{$__spacer}
+                                    {$__spacer}{$__spacer}<i>{$val_budg->remarks}{$__spacer}{$__spacer}{$__spacer}{$__spacer}
+                                    <b>PHP: {$val_budg->peso}</b>
+                                    {$__spacer}{$__spacer}
+                                    <b>USD: {$val_budg->dollar}</b></i>";
+
+                                # end of line items
+                                $__HTML.="</details>";
+                            }  
+                    }
+
+                    #end fy
+                    $__HTML.="</details>";
+                }
+
+                $__HTML.="</details>";
+            }
+
+            # end of FYP
+            $__HTML.="</details>";
+        }
+        
+        echo $__HTML;
+
     }
 
    
